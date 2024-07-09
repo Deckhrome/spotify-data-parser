8a889bf6c3289d54c90aba7d8a6e2e4cd5360a6f
Deckhrome <johan.verdon@gmail.com>
2024-07-09 11:44:11 +0000

    Structure changements

--- scripts/clean_CSV.py
+++ scripts/clean_CSV.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
+
 def clean_data(df):
-    '''
+    """
     Description: This function cleans the input DataFrame by performing the following operations:
     - Convert 'sp_track_popularity' to numeric and handle missing values
     - Convert emotion columns to percentage, round and convert to integer
@@ -21,63 +22,91 @@ def clean_data(df):
     Returns:
     ----------------
     df: The cleaned DataFrame.
-    '''
+    """
 
     print(f"Number of rows before cleaning: {df.shape[0]}")
 
     # Convert 'sp_track_popularity' to numeric and handle missing values
-    df['sp_track_popularity'] = pd.to_numeric(df['sp_track_popularity'], errors='coerce').astype('Int64')
+    df["sp_track_popularity"] = pd.to_numeric(
+        df["sp_track_popularity"], errors="coerce"
+    ).astype("Int64")
 
     # Convert emotion columns to percentage, round and convert to integer
-    emotion_columns = ['happiness_percentage', 'sadness_percentage', 'fear_percentage', 'anger_percentage']
+    emotion_columns = [
+        "happiness_percentage",
+        "sadness_percentage",
+        "fear_percentage",
+        "anger_percentage",
+    ]
     for col in emotion_columns:
-        df[col] = (pd.to_numeric(df[col], errors='coerce') * 100).round().astype('Int64')
+        df[col] = (
+            (pd.to_numeric(df[col], errors="coerce") * 100).round().astype("Int64")
+        )
 
     # Convert 'sp_track_duration' to seconds, round and convert to integer
-    df['sp_track_duration'] = (pd.to_numeric(df['sp_track_duration'], errors='coerce') / 1000).round().astype('Int64')
+    df["sp_track_duration"] = (
+        (pd.to_numeric(df["sp_track_duration"], errors="coerce") / 1000)
+        .round()
+        .astype("Int64")
+    )
 
     # Strip leading/trailing whitespace from all relevant columns
-    strip_columns = ['sp_uri', 'sp_track_name', 'sp_album_name', 'sp_artist_infos', 'genre_1', 'genre_2', 'genre_3']
+    strip_columns = [
+        "sp_uri",
+        "sp_track_name",
+        "sp_album_name",
+        "sp_artist_infos",
+        "genre_1",
+        "genre_2",
+        "genre_3",
+    ]
     df[strip_columns] = df[strip_columns].apply(lambda col: col.str.strip())
 
     # Drop rows with missing values in essential columns
-    essential_columns = ['sp_track_name', 'sp_album_name', 'sp_artist_infos', 'sp_uri']
+    essential_columns = ["sp_track_name", "sp_album_name", "sp_artist_infos", "sp_uri"]
     df.dropna(subset=essential_columns, inplace=True)
 
+    # Change uri value with the correct format : https://open.spotify.com/track/uri
+    df["sp_uri"] = df["sp_uri"].apply(lambda x: f"https://open.spotify.com/track/{x}")
+
     # Drop rows with duplicate 'sp_uri' values
-    df.drop_duplicates(subset='sp_uri', keep='first', inplace=True)
+    df.drop_duplicates(subset="sp_uri", keep="first", inplace=True)
 
     # Replace missing values in genre_2 and genre_3 with genre_1
-    df['genre_2'].fillna(df['genre_1'], inplace=True)
-    df['genre_3'].fillna(df['genre_1'], inplace=True)
+    df["genre_2"].fillna(df["genre_1"], inplace=True)
+    df["genre_3"].fillna(df["genre_1"], inplace=True)
 
     # Ensure 'emotion_code' is a string, filter by valid emotion codes
-    df['emotion_code'] = df['emotion_code'].astype(str)
-    valid_emotion_codes = ['0', '1', '2', '3']
-    df = df[df['emotion_code'].isin(valid_emotion_codes)]
+    df["emotion_code"] = df["emotion_code"].astype(str)
+    valid_emotion_codes = ["0", "1", "2", "3"]
+    df = df[df["emotion_code"].isin(valid_emotion_codes)]
 
     # Convert emotion codes to emotion names
-    emotion_mapping = {'0': 'happiness', '1': 'sadness', '2': 'anger', '3': 'fear'}
-    df['emotion_code'] = df['emotion_code'].replace(emotion_mapping)
+    emotion_mapping = {"0": "happiness", "1": "sadness", "2": "anger", "3": "fear"}
+    df["emotion_code"] = df["emotion_code"].replace(emotion_mapping)
+
+    # Print unique value for color
+    print(df["color"].unique())
 
     # Rename columns
     column_mapping = {
-        'sp_uri': 'uri',
-        'sp_track_name': 'track_name',
-        'sp_track_duration': 'track_duration',
-        'sp_track_popularity': 'track_popularity',
-        'sp_album_name': 'album_name',
-        'sp_artist_infos': 'artist_infos',
-        'emotion_code': 'emotion',
-        'genre_1': 'genre'
+        "sp_uri": "uri",
+        "sp_track_name": "track_name",
+        "sp_track_duration": "track_duration",
+        "sp_track_popularity": "track_popularity",
+        "sp_album_name": "album_name",
+        "sp_artist_infos": "artist_infos",
+        "emotion_code": "emotion",
+        "genre_1": "genre",
     }
     df.rename(columns=column_mapping, inplace=True)
 
     print(f"Number of rows after cleaning: {df.shape[0]}")
     return df
 
+
 def CSV_to_DF(path: str):
-    '''
+    """
     Description: This function reads the data from the CSV file and cleans it if necessary.
 
     Args:
@@ -87,13 +116,18 @@ def CSV_to_DF(path: str):
     Returns:
     ----------------
     df: The DataFrame containing the data from the CSV file.
-    '''
+    """
     try:
         print(path)
-        df = pd.read_csv(path, sep='\t', encoding='utf-8')
-        if not path.endswith('_clean.csv'):
+        df = pd.read_csv(path, sep="\t", encoding="utf-8")
+        if not path.endswith("_clean.csv"):
             df = clean_data(df)
-            df.to_csv(path.replace('.csv', '_clean.csv'), sep='\t', encoding='utf-8', index=False)
+            df.to_csv(
+                path.replace(".csv", "_clean.csv"),
+                sep="\t",
+                encoding="utf-8",
+                index=False,
+            )
         df.info()
     except Exception as e:
         print(f"Error: {e}")
--- scripts/main.py
+++ scripts/main.py
@@ -6,35 +6,40 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from scripts import parse_data, build_hierarchies, ClassManager
 
+
 def get_csv_path(filename):
-    """ Helper function to get the full path of a CSV file. """
+    """Helper function to get the full path of a CSV file."""
     return os.path.join(os.path.dirname(__file__), filename)
 
+
 def ensure_directory(directory):
-    """ Ensure the specified directory exists. """
+    """Ensure the specified directory exists."""
     os.makedirs(directory, exist_ok=True)
 
-def save_json(data, filepath):
-    """ Save data to a JSON file. """
-    with open(filepath, 'w') as f:
-        json.dump(data, f)
 
-full_path = '../data/full_table_27_aug.csv'
-full_path_clean = '../data/full_table_27_aug_clean.csv'
-sample_csv = '../data/sample_table.csv'
-sample_csv_clean = '../data/sample_table_clean.csv'
+def save_json(data, filepath):
+    """Save data to a JSON file."""
+    with open(filepath, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+full_path = "../data/csv_file/full_table_aug.csv"
+full_path_clean = "../data/csv_file/full_table_aug_clean.csv"
+sample_csv = "../data/csv_file/sample_table_aug.csv"
+sample_csv_clean = "../data/csv_file/sample_table_aug_clean.csv"
+
 
 def main():
     try:
         # Define paths
-        output_dir = '../build'
-        output_file = os.path.join(output_dir, 'full_data.json')
+        output_dir = "../build"
+        output_file = os.path.join(output_dir, "sample_data.json")
 
         # Initialize ClassManager
         class_manager = ClassManager()
 
         # Parse data
-        class_manager = parse_data(get_csv_path(full_path))
+        class_manager = parse_data(get_csv_path(sample_csv))
 
         # Build hierarchies
         build_hierarchies(class_manager)
@@ -52,5 +57,6 @@ def main():
     except Exception as e:
         print(f"An error occurred: {e}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
