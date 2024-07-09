2ef27231d0fc0eb6d639c5b7169f5cd232024d1b
Deckhrome <johan.verdon@gmail.com>
2024-07-09 15:14:24 +0000

    download unique img uri

--- scripts/data_aug.py
+++ scripts/data_aug.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import datetime
 import pandas as pd
 from tqdm import tqdm
+import os
 
 """
 This script adds image URIs and Timestamps to a CSV file.
@@ -12,33 +13,49 @@ def get_album_image_url(track_url):
     """
     Fetch the album image URL from the Spotify track page.
     """
-    response = requests.get(track_url)
-    html = response.text
-    soup = BeautifulSoup(html, features="html.parser")
-    album_img_url = soup.find("meta", property="og:image")["content"]
-    return album_img_url
+    try:
+        response = requests.get(track_url)
+        html = response.text
+        soup = BeautifulSoup(html, features="html.parser")
+        album_img_url = soup.find("meta", property="og:image")["content"]
+        return album_img_url
+    except Exception as e:
+        print(f"Error fetching album image URL: {e}")
+        return None
 
 def download_image(image_url, image_name):
     """
     Download the image from the given URL to the local directory.
     """
-    with open(image_name, "wb") as f:
-        f.write(requests.get(image_url).content)
+    try:
+        with open(image_name, "wb") as f:
+            f.write(requests.get(image_url).content)
+    except Exception as e:
+        print(f"Error downloading image: {e}")
 
-def add_image_uri_to_df(df):
+def add_image_uri_to_df(df, image_dir="../data/album_images/"):
     """
-    Add album image URIs to the DataFrame. And download the image to the local directory. (../data/album_images/)
+    Add album image URIs to the DataFrame and download the image to the local directory.
     """
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
+
     image_uris = []
+    downloaded_images = set(os.listdir(image_dir))
+    
     for uri in tqdm(df["sp_uri"]):
         track_url = f"https://open.spotify.com/track/{uri.split(':')[-1]}"
         image_url = get_album_image_url(track_url)
         if image_url:
-            # Download the image to the local directory
-            short_url = image_url[24:]
-            image_name = f"../data/album_images/{short_url}.jpg"
-            #download_image(image_url, image_name)
             # Remove https://i.scdn.co/image/ from the first part of uri
+            short_url = image_url[24:]
+            image_name = f"{image_dir}{short_url}.jpg"
+            image_file_name = f"{short_url}.jpg"
+            
+            if image_file_name not in downloaded_images:
+                download_image(image_url, image_name)
+                downloaded_images.add(image_file_name)
+
             image_uris.append(short_url)
         else:
             image_uris.append("")  # Or any default value if image URL is not found
